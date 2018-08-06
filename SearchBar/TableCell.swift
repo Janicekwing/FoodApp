8caa9945389098e52dcee3755ec4ffbba83ccc84
Janice He <janicekwhe@gmail.com>
2018-08-06 19:38:41 -0400

    Add scope titles

---
+++ SearchBar/TableCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableCell.swift
+//  SearchBar
+//
+//  Created by Janice He on 8/6/18.
+//  Copyright © 2018 Janice He. All rights reserved.
+//
+
+import UIKit
+
+class TableCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- SearchBar/ViewController.swift
+++ SearchBar/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
