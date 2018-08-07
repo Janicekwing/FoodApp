6c1bfe1d5e68e052e43355be3f5f120ab79a1f27
Janice He <janicekwhe@gmail.com>
2018-08-06 20:56:52 -0400

    omg yess it works! don't forget to connect your Table View to delegate AND data source haah

--- SearchBar/ViewController.swift
+++ SearchBar/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var tableView: UITableView!
     
@@ -18,14 +18,35 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpFood()
         // Do any additional setup after loading the view, typically from a nib.
     }
     
     private func setUpFood(){
-        foodArray.append(Food(name: "Hot Dog", type: FoodType(rawValue: "American")!, image: "<hotdog>"))
-        foodArray.append(Food(name: "Steak Frites", type: FoodType(rawValue: "French")!, image: "<steakfrites>"))
+        foodArray.append(Food(name: "Hot Dog", type: .american, image: "hotdog"))
+        foodArray.append(Food(name: "Steak Frites", type: .french, image: "steakfrites"))
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return foodArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as? TableCell else {
+                return UITableViewCell()
+        }
+        cell.dishName.text = foodArray[indexPath.row].name
+        cell.foodType.text = foodArray[indexPath.row].type.rawValue
+        cell.foodImage.image = UIImage(named:foodArray[indexPath.row].image)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
     }
 }
+
+
 
 class Food {
     let name: String
@@ -37,6 +58,7 @@ class Food {
         self.type = type
         self.image = image
     }
+    
 }
 
 enum FoodType: String {
