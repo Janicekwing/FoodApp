ad8feeb875b689663caef2d2a22f4c2ffce23dc0
Janice He <janicekwhe@gmail.com>
2018-08-07 22:02:39 -0400

    append more cell objects, testing live search

--- SearchBar/ViewController.swift
+++ SearchBar/ViewController.swift
@@ -8,13 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
 
     @IBOutlet weak var tableView: UITableView!
     
     @IBOutlet weak var searchBar: UISearchBar!
     
     var foodArray = [Food]()
+    var currentFoodArray = [Food]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,18 +26,26 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     private func setUpFood(){
         foodArray.append(Food(name: "Hot Dog", type: .american, image: "hotdog"))
         foodArray.append(Food(name: "Steak Frites", type: .french, image: "steakfrites"))
+        foodArray.append(Food(name: "Baguette", type: .french, image: "baguette"))
+        foodArray.append(Food(name: "Pizza", type: .american, image: "pizza"))
+        
+        currentFoodArray = foodArray
+    }
+    
+    private func setUpSearchBar(){
+        searchBar.delegate = self
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return foodArray.count
+        return currentFoodArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as? TableCell else {
                 return UITableViewCell()
         }
-        cell.dishName.text = foodArray[indexPath.row].name
-        cell.foodType.text = foodArray[indexPath.row].type.rawValue
+        cell.dishName.text = currentFoodArray[indexPath.row].name
+        cell.foodType.text = currentFoodArray[indexPath.row].type.rawValue
         cell.foodImage.image = UIImage(named:foodArray[indexPath.row].image)
         return cell
     }
@@ -44,6 +53,21 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 100
     }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        guard !searchText.isEmpty else{
+            currentFoodArray = foodArray
+            return
+        }
+        currentFoodArray.filter({ (Food) -> Bool in
+        Food.name.lowercased().contains(searchText.lowercased())
+        })
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, selectedScopeButtonIndexDidChange selectedScope: Int){
+        
+    }
+    
 }
 
 
